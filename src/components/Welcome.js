d0da1047f11a5fc89762940d4f6a8b1c65268f4b
Hugo Nogueira <hugomn@gmail.com>
2021-02-20 22:33:22 +0100

    Add wave emoji

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,8 +8,8 @@ import Avatar from './img/welcome_avatar.png';
 
 const Welcome = ({ author, className, langKey }) => {
   const getIntro = () => ({
-    en: `Hello! My name is <span class="name">Hugo</span>`,
-    pt: `Olá! Meu nome é <span class="name">Hugo</span>`,
+    en: `👋 Hello! My name is <span class="name">Hugo</span>`,
+    pt: `👋 Olá! Meu nome é <span class="name">Hugo</span>`,
   });
   const getBio = () => ({
     en:
